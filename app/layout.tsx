18764b0270f2c6694ff202191d9f9f3f63ea8ef4
Minh <khosharecodevn@gmail.com>
2026-03-03 20:50:49 +0700

    fix prerender error

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css"
 import { Navbar } from "@/components/navbar"
 import { Providers } from "@/components/providers"
 
+export const dynamic = "force-dynamic"  // 👈 THÊM DÒNG NÀY
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
